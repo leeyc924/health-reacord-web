7d82a5031ca01cbe31fd36afd4efe0ec0c5088ae
leeyc924 <leeyc924@naver.com>
2022-11-21 01:24:01 +0900

    create mock json

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DashboradPage() {
+  return (
+    <div>대시보드입니다</div>
+  )
+}
+
+export default DashboradPage;
---
+++ pages/record/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RecordPage() {
+  return (
+    <div>기록표입니다</div>
+  )
+}
+
+export default RecordPage;
