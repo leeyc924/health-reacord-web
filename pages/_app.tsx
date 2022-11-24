bb90c356863a1bd394cfd69bcda2bfa2beaba3d0
leeyc924 <leeyc924@naver.com>
2022-11-25 00:05:35 +0900

    reset css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+// App.getInitialProps = async ({ Component, ctx }: AppContext) => {
+// console.log('Component', Component);
+// console.log('ctx', ctx);
+// }
